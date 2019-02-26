4c4f685536d5fc00305789eb9434dd74abc0d229
Yuliang Jin <jyl198803@gmail.com>
2019-02-27 01:51:18 +0800

    fix nginx path

--- _nginx/create.sh
+++ _nginx/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --name gnainx -v /home/yuliang/gnailuy.com/_nginx/nginx.conf:/etc/nginx.conf -v /home/yuliang/webroot:/usr/local/nginx/html:ro -p 80:80 nginx
+docker run -d --name gnaiux -v /home/yuliang/gnailuy.com/_nginx/nginx.conf:/etc/nginx.conf:ro -v /home/yuliang/webroot:/usr/share/nginx/html:ro -p 80:80 nginx
 
