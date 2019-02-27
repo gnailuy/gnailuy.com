5c1f294dbfcc2b5efd9829f78981d5cf6285dcb2
Yuliang Jin <yuljin@microsoft.com>
2019-02-27 11:44:16 +0800

    fix nginx.conf

--- _nginx/create.sh
+++ _nginx/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --name gnaiux -v /home/yuliang/gnailuy.com/_nginx/conf/:/etc/nginx/:ro -v /home/yuliang/webroot:/usr/share/nginx/html:ro -p 80:80 nginx
+docker run -d --name gnaiux -v /home/yuliang/gnailuy.com/_nginx/conf/:/etc/nginx:ro -v /home/yuliang/webroot:/usr/share/nginx/html:ro -v /home/yuliang/logs:/var/log/nginx -p 80:80 nginx
 
