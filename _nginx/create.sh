76eaa4c9e45d0595b4454bdda5a45b76ecee53d4
Yuliang Jin <yuljin@microsoft.com>
2019-02-27 11:40:01 +0800

    update nginx configuration

--- _nginx/create.sh
+++ _nginx/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --name gnaiux -v /home/yuliang/gnailuy.com/_nginx/nginx.conf:/etc/nginx.conf:ro -v /home/yuliang/webroot:/usr/share/nginx/html:ro -p 80:80 nginx
+docker run -d --name gnaiux -v /home/yuliang/gnailuy.com/_nginx/conf/:/etc/nginx/:ro -v /home/yuliang/webroot:/usr/share/nginx/html:ro -p 80:80 nginx
 
